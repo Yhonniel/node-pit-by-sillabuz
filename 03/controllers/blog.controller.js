d238dac65199788b0091c3cb7c9af24a3b5c457d
Yahyr Paredes Arteaga <yahyrparedesarteaga@gmail.com>
2022-03-12 13:07:40 -0500

    fix

--- 03/controllers/blog.controller.js
+++ 03/controllers/blog.controller.js
@@ -1,15 +1,31 @@
-
 const blogRepository = require('./../repositories/blog.respository')
+const responseResult = require('./../utils/response.util')
+const httpStatusCodes = require('http-status-codes')
+
+
 
 const BlogController = {
 
-    async list(req, res, next){
-        const params = req.params
+    async list(req, res, next) {
 
-        const blogs = await blogRepository.list()
-        console.log(blogs)
-        res.send(blogs)
+        try {
+            // throw "Gato Malo"
+            const blogs = await blogRepository.list()
+            res.status(200).json(blogs)
+        } catch (exept){
+            res.status(500).send(exept)
+        }
+
+        // return responseResult.general(res, httpStatusCodes.OK, blogs)
     },
+    async detail(req, res, next) {
+        const {id} = req.params
+
+        const blog = await blogRepository.findById(id)
+
+        responseResult.general(res, httpStatusCodes.OK, blog)
+
+    }
 
 }
 

4628983a2f68f2668ce441b55cc2dcabda37afc9
Yahyr Paredes Arteaga <yahyrparedesarteaga@gmail.com>
2022-03-11 23:31:19 -0500

    fix

---
+++ dummy.js
@@ -0,0 +1,12 @@
+// JavaScrip -> Saben o tienen nociones Fuertes
+
+// Tipado Dinamico
+// let name; // undefinded
+// var lastName; // undefined
+// name = 'Conejo Malo'
+// console.log(name)
+// console.log(lastName)
+
+// var name = 'Juan'
+// let name = 'Pedro'
+// const name = 'pedro'
